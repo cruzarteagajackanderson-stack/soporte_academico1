40c3e471f44f3b4cde30717a6ab88829c5009d3e
cruzarteagajackanderson-stack <cruzarteagajackanderson@gmail.com>
2026-09-22 23:49:21 -0500

    Implementa requisito 5

--- programa.py
+++ programa.py
@@ -17,6 +17,18 @@ def mostrar_menu():
 
 
 
+def asignar_prioridad(tipo):
+    prioridades = {
+        "matrícula": "Alta",
+        "plataforma": "Alta",
+        "pagos": "Media",
+        "constancia": "Baja",
+        "otro": "Baja"
+    }
+
+    return prioridades.get(tipo.strip().lower(), "No definida")
+
+
 def registrar_solicitud():
     codigo = input("Código de estudiante: ")
 
@@ -37,11 +49,14 @@ def registrar_solicitud():
 
     descripcion = input("Descripción breve: ")
 
+    prioridad = asignar_prioridad(tipo)
+
     print("\nSolicitud registrada:")
     print("Código:", codigo)
     print("Nombre:", nombre)
     print("Tipo de consulta:", tipo)
     print("Descripción:", descripcion)
+    print("Prioridad:", prioridad)
 
 
 mostrar_menu()
