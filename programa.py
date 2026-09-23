974683477ab06af6b191f23b921528e1775186b3
cruzarteagajackanderson-stack <cruzarteagajackanderson@gmail.com>
2026-09-22 22:35:45 -0500

    Implementa requisito 1

---
+++ programa.py
@@ -0,0 +1,14 @@
+def registrar_solicitud():
+    codigo = input("Código de estudiante: ")
+    nombre = input("Nombre del estudiante: ")
+    tipo = input("Tipo de consulta: ")
+    descripcion = input("Descripción breve: ")
+
+    print("\nSolicitud registrada:")
+    print("Código:", codigo)
+    print("Nombre:", nombre)
+    print("Tipo:", tipo)
+    print("Descripción:", descripcion)
+
+
+registrar_solicitud()
