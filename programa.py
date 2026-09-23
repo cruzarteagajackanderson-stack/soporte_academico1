c70eb0fd8727f774d35f2523cd4034bf5422d133
cruzarteagajackanderson-stack <cruzarteagajackanderson@gmail.com>
2026-09-22 23:39:58 -0500

    Implementa requisito 2

--- programa.py
+++ programa.py
@@ -1,5 +1,16 @@
+
+def validar_codigo(codigo):
+    return codigo.strip() != "" and len(codigo.strip()) >= 6
+
+
+
 def registrar_solicitud():
     codigo = input("Código de estudiante: ")
+
+    if not validar_codigo(codigo):
+        print("Error: el código debe tener al menos 6 caracteres.")
+        return
+
     nombre = input("Nombre del estudiante: ")
     tipo = input("Tipo de consulta: ")
     descripcion = input("Descripción breve: ")
